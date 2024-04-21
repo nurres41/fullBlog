8f5f88ed9a941490d3e6be1a338d3a97e54a9580
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-21 22:12:29 +0200

    Featured and hero component created.

---
+++ components/home-page/hero.js
@@ -0,0 +1,18 @@
+import classes from './hero.module.css'
+import Image from 'next/image'
+
+const Hero = () => {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image src='/images/site/max.png' alt='An image showing user' width={300} height={300} />
+      </div>
+      <h1>Hi, I am Nuri</h1>
+      <p>
+        Bla bla
+      </p>
+    </section>
+  )
+}
+
+export default Hero
