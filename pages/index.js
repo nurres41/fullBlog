af68a68e10d05545b756015fdb048213c826524c
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-21 23:04:19 +0200

    Featured post component used by dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import Hero from '../components/home-page/hero'
+import FeaturedPost from '../components/home-page/ featured-post'
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-nextjs',
+    title: 'Getting Start',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NEXTJS',
+    date: '2022-02-10'
+  },
+  {
+    slug: 'nextjs-file-based-routing',
+    title: 'Getting Started',
+    image: 'nextjs-file-based-routing.png',
+    excerpt: 'NEXTJS',
+    date: '2022-02-11'
+  },
+];
 
 const Homepage = () => {
   return (
     <>
      <Hero /> 
+     <FeaturedPost posts={DUMMY_POSTS} />
     </>
   )
 }
