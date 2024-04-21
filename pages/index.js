2e4c24c11b8ab942745dc25115cc536a913cfdf3
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-21 22:12:44 +0200

    Asset added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from '../components/home-page/hero'
 
 const Homepage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+     <Hero /> 
+    </>
   )
 }
 
