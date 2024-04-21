4e329881de78c886ba747fe22282d357215cb066
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-21 23:16:51 +0200

    Code refactored

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -18,7 +18,7 @@ const PostItem = ({ post }) => {
     <li className={classes.post}>
       <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200} layout="responsive"/>
+          <Image src={imagePath} alt={title} width={300} height={200} />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import AllPosts from '../../components/posts/all-posts'
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-nextjs',
+    title: 'Getting Start',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NEXTJS',
+    date: '2022-02-10'
+  },
+  {
+    slug: 'nextjs-file-based-routing',
+    title: 'Getting Started',
+    image: 'nextjs-file-based-routing.png',
+    excerpt: 'NEXTJS',
+    date: '2022-02-11'
+  },
+];
 
 const AllPostsPage = () => {
   return (
-    <div>
-      
-    </div>
+    <AllPosts posts={DUMMY_POSTS} />
   )
 }
 
