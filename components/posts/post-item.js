8d44cab2c0d42586d7efc0b4e1c926f7f6a404c8
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-21 23:04:53 +0200

    Related post component refactored

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -11,13 +11,14 @@ const PostItem = ({ post }) => {
     year: 'numeric'
   })
 
-  const imagePath = `/images/posts/${slug}/${image}`
+  const linkPath = `/posts/${slug}/${image}`
+  const imagePath = `/images/posts/${image}`
   
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200}/>
+          <Image src={imagePath} alt={title} width={300} height={200} layout="responsive"/>
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -5,7 +5,7 @@ const PostsGrid = ({ posts }) => {
   return (
     <ul className={classes.grid}>
       {posts.map((post) => (
-        <PostItem />
+        <PostItem key={post.slug} post={post} />
       ))}
     </ul>
   );
