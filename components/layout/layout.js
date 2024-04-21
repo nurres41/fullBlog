ad7f524df3306f59334dadc75f5dedcd1009899f
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-21 22:24:24 +0200

    Layouts components and css files done

---
+++ components/layout/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainNavigation from './main-navigation'
+
+const Layout = (props) => {
+  return (
+    <>
+     <MainNavigation /> 
+     <main>
+        {props.children}
+     </main>
+    </>
+  )
+}
+
+export default Layout
---
+++ components/layout/main-navigation.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "./logo";
+import Link from "next/link";
+
+import classes from "./main-navigation.module.css";
+
+const MainNavigation = () => {
+  return (
+    <header className={classes.header}>
+      <Link href="/" passHref>
+        <Logo />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contract">Contract</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
