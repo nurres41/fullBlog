40fda84a8d5a0f1580957da32570710da4035f21
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-27 23:14:34 +0200

    Markdown extension used

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,12 +1,14 @@
 import classes from './post-content.module.css'
 import PostHeader from './post-header';
+// Rendering markdown as JSX
+import ReactMarkdown from 'react-markdown'
 
 const DUMMY_POSTS = 
     {
       slug: 'getting-started-nextjs',
       title: 'Getting Start',
       image: 'getting-started-nextjs.png',
-      content: 'NEXTJS',
+      content: '# NEXTJS',
       date: '2022-02-10'
     }
   ;
@@ -18,7 +20,7 @@ const PostContent = () => {
   return (
     <article  className={classes.content}>
       <PostHeader title={DUMMY_POSTS?.title} image={imagePath} />
-      {DUMMY_POSTS.content}
+      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
     </article>
   )
 }
