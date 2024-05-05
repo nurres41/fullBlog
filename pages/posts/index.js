62b4d69e9e4c30c17dcb90a088b7dd6c36ad5f17
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-05-05 20:33:50 +0200

    Dummy data deleted

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -2,23 +2,6 @@ import React from 'react'
 import AllPosts from '../../components/posts/all-posts'
 import { getAllPosts } from '../../lib/posts-util';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-started-nextjs',
-    title: 'Getting Start',
-    image: 'getting-started-nextjs.png',
-    excerpt: 'NEXTJS',
-    date: '2022-02-10'
-  },
-  {
-    slug: 'nextjs-file-based-routing',
-    title: 'Getting Started',
-    image: 'nextjs-file-based-routing.png',
-    excerpt: 'NEXTJS',
-    date: '2022-02-11'
-  },
-];
-
 const AllPostsPage = (props) => {
   return (
     <AllPosts posts={props.posts} />
