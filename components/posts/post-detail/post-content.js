e6f6945e230c16349efc34c4a89e5231dc3fe8ad
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-05-05 20:33:41 +0200

    Updated according .md. We can override

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,28 +1,67 @@
-import classes from './post-content.module.css'
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 import PostHeader from './post-header';
-// Rendering markdown as JSX
-import ReactMarkdown from 'react-markdown'
+import classes from './post-content.module.css';
 
-const DUMMY_POSTS = 
-    {
-      slug: 'getting-started-nextjs',
-      title: 'Getting Start',
-      image: 'getting-started-nextjs.png',
-      content: '# NEXTJS',
-      date: '2022-02-10'
-    }
-  ;
+function PostContent(props) {
+  const { post } = props;
 
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POSTS.slug}/${DUMMY_POSTS.image}`
-  
-  console.log("🚀 ~ PostContent ~ imagePath:", imagePath)
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
-    <article  className={classes.content}>
-      <PostHeader title={DUMMY_POSTS?.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
+    <article className={classes.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
-  )
+  );
 }
 
-export default PostContent
+export default PostContent;
