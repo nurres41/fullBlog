9c0a7f924c082472154b7b26be5198401b8b4876
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-05-05 20:34:13 +0200

    Pre render used

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,34 @@
-import React from 'react'
-import PostContent from '../../components/posts/post-detail/post-content'
+import React from "react";
+import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostFiles } from "../../lib/posts-util";
 
-const SinglePostPage = () => {
-  return (
-    <PostContent />
-  )
+const SinglePostPage = (props) => {
+  return <PostContent post={props.post} />;
+};
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
 }
 
-export default SinglePostPage
+export function getStaticPaths() {
+  const postFileNames = getPostFiles();
+
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    // paths return obj instead of array
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
+}
+
+export default SinglePostPage;
