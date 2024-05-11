23c5f3701da2fda57cfbcadb5b1fe5b1f60bfeb6
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-05-12 00:07:20 +0200

    contact form added

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,28 +1,132 @@
+import { useEffect, useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
+
+async function sendContactData(contactDetails) {
+  const response = fetch("/api/contact", {
+    method: "POST",
+    body: JSON.stringify(contactDetails),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = (await response).json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+}
 
 const ContactForm = () => {
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredMessage, setEnteredMessage] = useState("");
+  const [requestStatus, setRequestStatus] = useState();
+  const [requestError, setRequestError] = useState();
+
+  useEffect(() => {
+    if (requestStatus === 'pending' || requestStatus === 'error') {
+      const timer = setTimeout(() => {
+        setRequestStatus(null)
+        setRequestError(null)
+      },3000)
+
+      return () => clearTimeout(timer)
+    } 
+  }, [requestStatus])
+
+  const sendMessageHandler = async (e) => {
+    e.preventDefault();
+
+    setRequestStatus("pending");
+
+    try {
+      await sendContactData({
+        name: enteredName,
+        email: enteredEmail,
+        message: enteredMessage,
+      });
+      setRequestStatus("success");
+      setEnteredEmail('')
+      setEnteredName('')
+      setEnteredMessage('')
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus("error");
+    }
+  };
+
+  let notification;
+
+  switch (requestStatus) {
+    case "pending":
+      notification = {
+        status: "pending",
+        title: "Sending message...",
+        message: "Your message is on its way",
+      };
+    case "success":
+      notification = {
+        status: "success",
+        title: "Success",
+        message: "Your message is on its way",
+      };
+    case "error":
+      notification = {
+        status: "error",
+        title: "Failed",
+        message: requestError,
+      };
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your E-mail</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={enteredEmail}
+              onChange={(event) => setEnteredEmail(event.target.value)}
+            />
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Your Name</label>
-            <input type="text" id="name" required />
+            <input
+              type="text"
+              id="name"
+              required
+              value={enteredName}
+              onChange={(event) => setEnteredName(event.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea id="message" rows="5" />
+          <textarea
+            id="message"
+            required
+            rows="5"
+            value={enteredMessage}
+            onChange={(event) => setEnteredMessage(event.target.value)}
+          />
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
         </div>
       </form>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   );
 };
