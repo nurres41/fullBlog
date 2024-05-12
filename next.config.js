1ebf73456e74b962d8c6ceba40309c1b275b8595
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-05-12 16:05:06 +0200

    config added

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+        mongodb_username: 'nurionurkurtulus',
+        mongodb_password: 'Muckmuck41.',
+        mongodb_clustername: 'cluster0',
+        mongodb_database: 'my-site',
+    }
+}
